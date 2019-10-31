5cc1bd5903c927c1b723fac3e79b0461a4537df0
Daniel Wilder <dauncy@gmail.com>
2019-10-31 10:49:11 -0700

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,6 @@
 class Project
     attr_reader :title
-    attr_accessor :backers 
+    attr_accessor :backers #attr_accessor required to use in backer.rb
     
     def initialize(title)
         @title = title
@@ -8,7 +8,7 @@ class Project
         
     end 
 
-    def add_backer(backer)
+    def add_backer(backer) #Passing instance of Backer. (backer) = object
         @backers << backer
         backer.backed_projects << self
     end 
